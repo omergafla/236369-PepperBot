ee6ce3235445034826cca8aff03f2d87a6ca1384
omergafla <omergafla@gmail.com>
2022-01-05 23:44:12 +0200

    set spinners for cards

--- my-app/src/Components/BasicGrid/BasicGrid.tsx
+++ my-app/src/Components/BasicGrid/BasicGrid.tsx
@@ -3,26 +3,49 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import BasicCard from '../Cards/BasicCard/BasicCard';
 import styles from './BasicGrid.module.css'
+import { useState } from 'react';
 
+export interface loaders{
+  [key: string]: boolean;
+}
 
 export default function BasicGrid(props: any) {
+  const init_loaders = {
+    users: true,
+    active_users: true,
+    polls: true,
+    today_polls: true,
+    admins: true,
+    active_users_today: true,
+    popular_poll: true,
+    newest_poll: true,
+    daily_users: true,
+    daily_polls: true,
+    active_users_ratio: true,
+    most_answered_polls: true,
+    admins_compare: true,
+    most_clear: true,
+    unclear: true
+  }
+
+  const [loaders, setLoaders] = useState<loaders>(init_loaders)
   const { size } = props;
   return (
     <div className={styles.dashboard}>
-        <div className={styles.div1}> <BasicCard data={"89"} label={"Users"} /> </div>
-        <div className={styles.div2}> <BasicCard data={"80"} label={"Active Users"} /></div>
-        <div className={styles.div3}> <BasicCard data={"29"} label={"Polls"} /></div>
-        <div className={styles.div4}> <BasicCard data={"4"} label={"New Polls Today"} /></div>
-        <div className={styles.div5}> <BasicCard data={"3"} label={"Admins"} /></div>
-        <div className={styles.div6}> <BasicCard data={"70"} label={"Active Users Today"} /></div>
-        <div className={styles.div7}> <BasicCard data={"#23 How are you?"} /></div>
-        <div className={styles.div8}> <BasicCard data={"#45 Where is the best pizza in Haifa?"} /></div>
-        <div className={styles.div9}> <BasicCard data={"#45 Where is the best pizza in Haifa?"} /></div>
-        <div className={styles.div10}> <BasicCard data={"#45 Where is the best pizza in Haifa?"} /> </div>
-        <div className={styles.div11}> <BasicCard data={"#45 Where is the best pizza in Haifa?"} /> </div>
-        <div className={styles.div12}> <BasicCard data={"#45 Where is the best pizza in Haifa?"} /> </div>
-        <div className={styles.div13}> <BasicCard data={"#45 Where is the best pizza in Haifa?"} /> </div>
-        <div className={styles.div14}> <BasicCard data={"#45 Where is the best pizza in Haifa?"} /> </div>
+        <div className={styles.div1}> <BasicCard loading={loaders.users} data={"89"} label={"Users"} /> </div>
+        <div className={styles.div2}> <BasicCard loading={loaders.active_users} data={"80"} label={"Active Users"} /></div>
+        <div className={styles.div3}> <BasicCard loading={loaders.polls} data={"29"} label={"Polls"} /></div>
+        <div className={styles.div4}> <BasicCard loading={loaders.today_polls} data={"4"} label={"New Polls Today"} /></div>
+        <div className={styles.div5}> <BasicCard loading={loaders.admins} data={"3"} label={"Admins"} /></div>
+        <div className={styles.div6}> <BasicCard loading={loaders.active_users_today} data={"70"} label={"Active Users Today"} /></div>
+        <div className={styles.div7}> <BasicCard loading={loaders.popular_poll} data={"#23 How are you?"}  label={"Most Popular Poll"}/></div>
+        <div className={styles.div8}> <BasicCard loading={loaders.newest_poll} data={"#45 Where is the best pizza in Haifa?"} label={"Newest Poll"} /></div>
+        <div className={styles.div9}> <BasicCard loading={loaders.daily_users} data={"#45 Where is the best pizza in Haifa?"}  label={"New Users per Day"}/></div>
+        <div className={styles.div10}> <BasicCard loading={loaders.daily_polls} data={"#45 Where is the best pizza in Haifa?"}  label={"New Polls per Day"}/></div>
+        <div className={styles.div11}> <BasicCard loading={loaders.active_users_ratio} data={"#45 Where is the best pizza in Haifa?"} label={"Active Users Ratio"}/>  </div>
+        <div className={styles.div12}> <BasicCard loading={loaders.most_answered_polls} data={"#45 Where is the best pizza in Haifa?"} label={"Most Popular Polls"}/> </div>
+        <div className={styles.div13}> <BasicCard loading={loaders.admins_compare} data={"#45 Where is the best pizza in Haifa?"} label={"Admins Activity"}/> </div>
+        <div className={styles.div14}> <BasicCard loading={loaders.most_clear} data={"#45 Where is the best pizza in Haifa?"} label={"Easiest Decision"}/> </div>
     </div>
   );
 }
--- my-app/src/Components/Cards/BasicCard/BasicCard.tsx
+++ my-app/src/Components/Cards/BasicCard/BasicCard.tsx
@@ -7,15 +7,16 @@ import { useState } from 'react';
 
 
 export default function BasicCard(props: any) {
-  const { label, data } = props;
+  const { label, data, loading } = props;
+
   return (
     <Card className={styles.BasicCard} sx={{borderRadius: "16px"}}>
       <CardContent>
-        <Typography variant="h5" component="div" align="center" gutterBottom>
-          {data}
-        </Typography>
-        <Typography variant="h5" component="div" align="center">
+        <Typography variant="h6" component="div" align="center">
           {label}
+        </Typography>
+        <Typography variant="h5" component="div" align="center" gutterBottom>
+          {loading ? (<img src={require('./spinner.gif')} height="70px"></img>) : data}
         </Typography>
       </CardContent>
     </Card>
